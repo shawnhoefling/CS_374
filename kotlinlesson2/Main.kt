3ca894bbeb7718cbdebd80221a94f598ab524488
ShawnH <tylerh1098@gmail.com>
2024-04-02 12:11:03 -0400

    functions part 2

--- kotlinlesson2/Main.kt
+++ kotlinlesson2/Main.kt
@@ -1,3 +1,14 @@
 fun main(args: Array<String>) {
-    println("hello, world, for the 400th time")
+    // will assign kotlin.unit
+    val isUnit = println("This is an expression")
+    println(isUnit)
+
+    val temperature = 10
+    val isHot = if(temperature > 50) true else false
+    println(isHot)
+
+    val temperature2 = 10
+    val message = "The water temperature is ${if(temperature2 > 50) "too warm" else "OK"}."
+    println(message)
+
 }
