6cfb81b0935139af3be99a3d878ae4a719cac59c
ShawnH <tylerh1098@gmail.com>
2024-04-02 12:36:46 -0400

    Further work on functions part 2

--- kotlinlesson2/Main.kt
+++ kotlinlesson2/Main.kt
@@ -1,5 +1,6 @@
-fun main(args: Array<String>) {
+import java.util.* // required
     // will assign kotlin.unit
+    /*
     val isUnit = println("This is an expression")
     println(isUnit)
 
@@ -10,5 +11,25 @@ fun main(args: Array<String>) {
     val temperature2 = 10
     val message = "The water temperature is ${if(temperature2 > 50) "too warm" else "OK"}."
     println(message)
+    */
 
-}
+    fun randomDay() : String {
+        val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+        return week[Random().nextInt(week.size)]
+    }
+
+    fun randomFood() : String {
+        val foodType = arrayOf ("pellets", "insects", "wet food")
+        return foodType[Random().nextInt(foodType.size)]
+    }
+
+    fun feedTheFish() {
+        val day = randomDay()
+        val food = randomFood()
+        println("Today is $day and the fish eat $food")
+    }
+
+    fun main(args: Array<String>) {
+        feedTheFish()
+    }
+
