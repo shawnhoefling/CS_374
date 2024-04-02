e713cf05669aef2d68391fc4f9e0868b6d74d2e1
ShawnH <tylerh1098@gmail.com>
2024-04-02 16:39:57 -0400

    continued work

--- kotlinlesson2/Main.kt
+++ kotlinlesson2/Main.kt
@@ -13,6 +13,7 @@ import java.util.* // required
     println(message)
     */
 
+
     fun randomDay() : String {
         val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
         return week[Random().nextInt(week.size)]
@@ -30,10 +31,25 @@ import java.util.* // required
         return food
     }
 
+
+    fun swim(speed: String = "fast") {
+        println("swimming $speed")
+    }
+
+    fun shouldChangeWater(day: String, temperature: Int = 22, dirty: Int = 20): Boolean {
+        return when {
+            temperature > 30 -> true
+            dirty > 30 -> true
+            day == "Sunday" -> true
+            else -> false
+        }
+    }
+
     fun feedTheFish() {
         val day = randomDay()
         val food = fishFood(day)
-        println("> Today is $day and the fish eat $food")
+        println("Today is $day and the fish eat $food")
+        println("Change water: ${shouldChangeWater(day)}")
     }
 
     fun main(args: Array<String>) {
