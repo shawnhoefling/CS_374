42d8ed9136f43df5d68ee27fdb741c227c5311a1
ShawnH <tylerh1098@gmail.com>
2024-04-03 16:18:39 -0400

    part 6 lesson 3

--- kotlinlesson3/src/main/kotlin/example/myapp/AquariumFish.kt
+++ kotlinlesson3/src/main/kotlin/example/myapp/AquariumFish.kt
@@ -1,5 +1,24 @@
 package example.myapp
 
-abstract class AquariumFish {
-    abstract val color: String
+class Shark: AquariumFish(), FishAction {
+    override val color = "color: BLACK"
+    override fun eat() {
+        println("hunt and eat fish")
+    }
+}
+
+class Plecostomus: AquariumFish(), FishAction {
+    override val color = "color: GOLD"
+    override fun eat() {
+        println("eat algae")
+    }
+}
+
+interface FishAction {
+    fun eat()
+}
+
+abstract class AquariumFish : FishAction {
+    abstract val color : String
+    override fun eat() = println("yum")
 }
--- kotlinlesson3/src/main/kotlin/example/myapp/Main.kt
+++ kotlinlesson3/src/main/kotlin/example/myapp/Main.kt
@@ -1,13 +1,19 @@
 package example.myapp
 
-fun buildAquarium() {
-    val myAquarium = Aquarium(width = 25, length = 25, height = 40)
-    myAquarium.printSize()
-    val myTower = TowerTank(diameter = 25, height = 40)
-    myTower.printSize()
+fun makeFish() {
+    val shark = Shark()
+    val pleco = Plecostomus()
+
+
+    println("Fish Types")
+    println("-----------")
+    println("Shark ${shark.color}")
+    shark.eat()
+    println("Plecostomous ${pleco.color}")
+    pleco.eat()
 }
 
 fun main() {
-    buildAquarium()
+    makeFish()
 }
 
