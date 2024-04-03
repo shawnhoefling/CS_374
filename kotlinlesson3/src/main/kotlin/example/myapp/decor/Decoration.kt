6a7a952798adf9ef1c95a47a4ca763ea1f8c31ae
ShawnH <tylerh1098@gmail.com>
2024-04-03 17:05:04 -0400

    part 8 lesson 3

---
+++ kotlinlesson3/src/main/kotlin/example/myapp/decor/Decoration.kt
@@ -0,0 +1,20 @@
+package example.myapp.decor
+
+
+data class Decoration2(val rocks: String, val wood: String, val diver: String) {
+
+}
+
+fun makeDecorations() {
+
+    val d5 = Decoration2("crystal", "wood", "diver")
+    println(d5)
+    val (rock, wood, diver) = d5
+    println(rock)
+    println(wood)
+    println(diver)
+}
+
+fun main() {
+    makeDecorations()
+}
