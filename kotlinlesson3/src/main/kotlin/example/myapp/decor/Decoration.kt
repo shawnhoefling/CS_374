3e17004707b1571b25815f1d6379a292e6a45de8
ShawnH <tylerh1098@gmail.com>
2024-04-03 17:11:00 -0400

    Google Codelabs Kotlin L3

--- kotlinlesson3/src/main/kotlin/example/myapp/decor/Decoration.kt
+++ kotlinlesson3/src/main/kotlin/example/myapp/decor/Decoration.kt
@@ -15,6 +15,10 @@ fun makeDecorations() {
     println(diver)
 }
 
+enum class Color(val rgb: Int) {
+    RED(0xFF0000), GREEN(0x00FF00), BLUE(0x0000FF);
+}
+
 fun main() {
     makeDecorations()
 }
