8d58d14662e7d8748eb1371309f4022bd5e63380
ShawnH <tylerh1098@gmail.com>
2024-04-03 15:02:00 -0400

    part 3 lesson 3

--- kotlinlesson3/src/main/kotlin/example/myapp/Aquarium.kt
+++ kotlinlesson3/src/main/kotlin/example/myapp/Aquarium.kt
@@ -1,15 +1,30 @@
 package example.myapp
 
-class Aquarium (length:Int = 3000, width: Int = 2000, height: Int = 2500) {
+class Aquarium (var length:Int = 100, var width: Int = 20, var height: Int = 40) {
     // dimensions are in cm
-    var width: Int = width
-    var height: Int = height
-    var length: Int = length
+    init {
+        println("---------------------")
+        println("> aquarium initializing")
+        println("---------------------")
+    }
+    constructor(numberOfFish: Int) : this() {
+        // 2000 cm^3 per fish
+        val tank = numberOfFish * 2000 * 1.1
+        // calculating height needed
+        height = (tank / (length * width)).toInt()
+    }
 
+    var volume: Int
+        get() = width * height * length / 1000 // 1000 cm^3 = 1 liter
+        set(value) {
+            height = (value * 1000) / (width * length)
+        }
 
     fun printSize() {
         println("Width: $width cm " +
                 "Length: $length cm " +
                 "Height: $height cm ")
+        // 1 liter = 1000 cm^3
+        println("Volume: $volume liters")
     }
 }
--- kotlinlesson3/src/main/kotlin/example/myapp/Main.kt
+++ kotlinlesson3/src/main/kotlin/example/myapp/Main.kt
@@ -1,10 +1,21 @@
 package example.myapp
 
 fun buildAquarium() {
-    val myAquarium = Aquarium()
-    myAquarium.printSize()
-    myAquarium.height = 60
-    myAquarium.printSize()
+    // mavis = The Mavis Aquarium
+    val mavis = Aquarium()
+    mavis.printSize()
+    val mavis2 = Aquarium(width = 25)
+    mavis2.printSize()
+    val mavis3 = Aquarium(height = 50, length = 100)
+    mavis3.printSize()
+    val mavis4 = Aquarium(width = 400, height = 500, length = 1000)
+    mavis4.printSize()
+
+    // for secondary constructor
+    val mavis5 = Aquarium(numberOfFish = 40)
+    mavis5.printSize()
+    mavis5.volume = 60
+    mavis5.printSize()
 }
 
 fun main() {
