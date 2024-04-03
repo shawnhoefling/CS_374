9091b5902fca1a8e88e8df754133ec04d7698259
ShawnH <tylerh1098@gmail.com>
2024-04-03 15:30:49 -0400

    part 5 lesson 3

--- kotlinlesson3/src/main/kotlin/example/myapp/Main.kt
+++ kotlinlesson3/src/main/kotlin/example/myapp/Main.kt
@@ -1,19 +1,10 @@
 package example.myapp
 
 fun buildAquarium() {
-    // mavis = The Mavis Aquarium
-    val mavis = Aquarium()
-    mavis.printSize()
-    val mavis2 = Aquarium(width = 25)
-    mavis2.printSize()
-    val mavis3 = Aquarium(height = 50, length = 100)
-    mavis3.printSize()
-    val mavis4 = Aquarium(width = 400, height = 500, length = 1000)
-    mavis4.printSize()
-
-    // for secondary constructor
-    val mavis5 = Aquarium(length = 25, width = 25, height = 40)
-    mavis5.printSize()
+    val myAquarium = Aquarium(width = 25, length = 25, height = 40)
+    myAquarium.printSize()
+    val myTower = TowerTank(diameter = 25, height = 40)
+    myTower.printSize()
 }
 
 fun main() {
