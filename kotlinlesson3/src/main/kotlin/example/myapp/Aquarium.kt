32a48529795568e457399bbc0cafbbddecaffbb8
ShawnH <tylerh1098@gmail.com>
2024-04-03 13:37:34 -0400

    part 2 lesson 3

--- kotlinlesson3/src/main/kotlin/example/myapp/Aquarium.kt
+++ kotlinlesson3/src/main/kotlin/example/myapp/Aquarium.kt
@@ -1,4 +1,14 @@
 package example.myapp
 
 class Aquarium {
+    var width: Int = 20
+    var height: Int = 40
+    var length: Int = 100
+
+
+    fun printSize() {
+        println("Width: $width cm " +
+                "Length: $length cm " +
+                "Height: $height cm ")
+    }
 }
---
+++ kotlinlesson3/src/main/kotlin/example/myapp/Main.kt
@@ -0,0 +1,13 @@
+package example.myapp
+
+fun buildAquarium() {
+    val myAquarium = Aquarium()
+    myAquarium.printSize()
+    myAquarium.height = 60
+    myAquarium.printSize()
+}
+
+fun main() {
+    buildAquarium()
+}
+
