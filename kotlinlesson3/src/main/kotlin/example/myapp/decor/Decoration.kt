589ab382833eb401e6bcc8329426d91d0ce19e16
ShawnH <tylerh1098@gmail.com>
2024-04-03 17:20:00 -0400

    part 1 lesson 3.5

--- kotlinlesson3/src/main/kotlin/example/myapp/decor/Decoration.kt
+++ kotlinlesson3/src/main/kotlin/example/myapp/decor/Decoration.kt
@@ -19,6 +19,15 @@ enum class Color(val rgb: Int) {
     RED(0xFF0000), GREEN(0x00FF00), BLUE(0x0000FF);
 }
 
+class Choice {
+    companion object {
+        var name: String = "lyric"
+        fun showDescription(name: String) = println("My favorite $name")
+    }
+}
+
 fun main() {
-    makeDecorations()
+    println(Choice.name)
+    Choice.showDescription("pick")
+    Choice.showDescription("selection")
 }
